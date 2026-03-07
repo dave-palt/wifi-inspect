a5e5a960bcfad9f7604f3974be9e188a9bd96cbd
Davide Palchetti <davide.palchetti@gmail.com>
2026-03-07 22:56:34 +1100

    Fix: Remove android wrapper, use top-level configurations.all

--- mobile/plugins/forceMedia3Version.js
+++ mobile/plugins/forceMedia3Version.js
@@ -3,25 +3,24 @@ const { withAppBuildGradle } = require('@expo/config-plugins');
 module.exports = function forceMedia3Version(config) {
   return withAppBuildGradle(config, (config) => {
     const resolutionStrategy = `
-android {
-    configurations.all {
-        resolutionStrategy {
-            force 'androidx.media3:media3-exoplayer:1.4.1'
-            force 'androidx.media3:media3-ui:1.4.1'
-            force 'androidx.media3:media3-common:1.4.1'
-            force 'androidx.media3:media3-datasource:1.4.1'
-            force 'androidx.media3:media3-datasource-okhttp:1.4.1'
-            force 'androidx.media3:media3-decoder:1.4.1'
-            force 'androidx.media3:media3-extractor:1.4.1'
-            force 'androidx.media3:media3-container:1.4.1'
-            force 'androidx.media3:media3-database:1.4.1'
-            force 'androidx.media3:media3-session:1.4.1'
-            force 'androidx.media3:media3-exoplayer-dash:1.4.1'
-            force 'androidx.media3:media3-exoplayer-hls:1.4.1'
-            force 'androidx.media3:media3-exoplayer-smoothstreaming:1.4.1'
-        }
+configurations.all {
+    resolutionStrategy {
+        force 'androidx.media3:media3-exoplayer:1.4.1'
+        force 'androidx.media3:media3-ui:1.4.1'
+        force 'androidx.media3:media3-common:1.4.1'
+        force 'androidx.media3:media3-datasource:1.4.1'
+        force 'androidx.media3:media3-datasource-okhttp:1.4.1'
+        force 'androidx.media3:media3-decoder:1.4.1'
+        force 'androidx.media3:media3-extractor:1.4.1'
+        force 'androidx.media3:media3-container:1.4.1'
+        force 'androidx.media3:media3-database:1.4.1'
+        force 'androidx.media3:media3-session:1.4.1'
+        force 'androidx.media3:media3-exoplayer-dash:1.4.1'
+        force 'androidx.media3:media3-exoplayer-hls:1.4.1'
+        force 'androidx.media3:media3-exoplayer-smoothstreaming:1.4.1'
     }
 }
+
 `;
 
     if (!config.modResults.contents.includes("resolutionStrategy {")) {
